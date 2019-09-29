d0c7dc04e2f2b6395fef19ebcd28f60e1d127d62
Eric Laitman <elaitman1@gmail.com>
2019-09-29 12:01:35 -0700

    edit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+
+    </div>
+  );
+}
+
+export default App;
